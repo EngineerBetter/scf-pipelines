ed6e7270e361a3f1c5a57beff562b49715e51114
Daniel Jones <daniel.jones@engineerbetter.com>
2019-01-17 19:24:57 +0000

    force destroy non-empty bucket, coz that'll be safe

--- aws/tf/ci/bucket.tf
+++ aws/tf/ci/bucket.tf
@@ -1,10 +1,15 @@
 variable "region" {}
 
+variable "force-destroy" {
+  default = false
+}
+
 provider "aws" {
   region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "ci" {
-  bucket = "scf-pipeline"
-  acl    = "private"
+  bucket        = "scf-pipeline"
+  acl           = "private"
+  force_destroy = "${var.force-destroy}"
 }
